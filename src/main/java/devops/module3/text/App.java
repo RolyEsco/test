37dae7472a3dedee5efc28db80dbc7e365dda2f8
roly.esco <roly.esco@gmail.com>
2022-02-07 04:34:24 -0400

    inicializando prueba modulo 3 DEVOPS

---
+++ src/main/java/devops/module3/text/App.java
@@ -0,0 +1,13 @@
+package devops.module3.text;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "hola roly!" );
+    }
+}
